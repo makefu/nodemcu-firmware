053f9cfb100127f5d2d1860e94a8eba7087aa230
makefu <github@syntax-fehler.de>
2015-06-10 08:10:38 +0200

    remove legacy code

--- app/modules/ws2812.c
+++ app/modules/ws2812.c
@@ -28,62 +28,6 @@ static void ICACHE_FLASH_ATTR send_ws_1(uint8_t gpio) {
   i = 6; while (i--) GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << gpio);
 }
 
-// Lua: ws2812.writergb(pin, "string")
-// Byte triples in the string are interpreted as R G B values and sent to the hardware as G R B.
-// WARNING: this function scrambles the input buffer :
-//    a = string.char(255,0,128)
-//    ws212.writergb(3,a)
-//    =a.byte() 
-//    (0,255,128)
-
-// ws2812.writergb(4, string.char(255, 0, 0)) uses GPIO2 and sets the first LED red.
-// ws2812.writergb(3, string.char(0, 0, 255):rep(10)) uses GPIO0 and sets ten LEDs blue.
-// ws2812.writergb(4, string.char(0, 255, 0, 255, 255, 255)) first LED green, second LED white.
-static int ICACHE_FLASH_ATTR ws2812_writergb(lua_State* L)
-{
-  const uint8_t pin = luaL_checkinteger(L, 1);
-  size_t length;
-  const char *rgb = luaL_checklstring(L, 2, &length);
-  // dont modify lua-internal lstring - make a copy instead
-  char *buffer = (char *)c_malloc(length);
-  c_memcpy(buffer, rgb, length);
-
-  // Initialize the output pin:
-  platform_gpio_mode(pin, PLATFORM_GPIO_OUTPUT, PLATFORM_GPIO_FLOAT);
-  platform_gpio_write(pin, 0);
-
-  // Ignore incomplete Byte triples at the end of buffer:
-  length -= length % 3;
-
-  // Rearrange R G B values to G R B order needed by WS2812 LEDs:
-  size_t i;
-  for (i = 0; i < length; i += 3) {
-    const char r = buffer[i];
-    const char g = buffer[i + 1];
-    buffer[i] = g;
-    buffer[i + 1] = r;
-  }
-
-  // Do not remove these:
-  os_delay_us(1);
-  os_delay_us(1);
-
-  // Send the buffer:
-  os_intr_lock();
-  for (i = 0; i < length; i++) {
-    uint8_t mask = 0x80;
-    while (mask) {
-      (buffer[i] & mask) ? send_ws_1(pin_num[pin]) : send_ws_0(pin_num[pin]);
-      mask >>= 1;
-    }
-  }
-  os_intr_unlock();
-
-  c_free(buffer);
-
-  return 0;
-}
-
 // Lua: ws2812.write(pin, "string")
 // Byte triples in the string are interpreted as G R B values.
 // This function does not corrupt your buffer.
